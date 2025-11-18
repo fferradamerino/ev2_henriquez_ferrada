909632910048d088fe57cdb1eebf95598840e942
fferradamerino <156674516+fferradamerino@users.noreply.github.com>
2025-11-18 16:49:26 +0000

    Añaidr bootstrap

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import "./css/bootstrap.min.css";
 
 export function App() {
   return (
-    <Fragment>
-      <Steam />
-    </Fragment>
+    <>
+      <form>
+        <div class="form-group">
+          <label for="card">Número de carta</label>
+          <input type="text" class="form-control" id="card" placeholder="Ingrese su carta" />
+        </div>
+        <button type="submit" class="btn btn-primary">Añadir carta</button>
+      </form>
+    </>
   );
 }
